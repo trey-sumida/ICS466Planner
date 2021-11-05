52f6a7d5c4182ce5f23e9d15faec302fac04e685
trey-sumida <tasumida@hawaii.edu>
2021-11-04 22:35:40 -1000

    added button

--- ui/screens/NotesListScreen.js
+++ ui/screens/NotesListScreen.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default function NotesListScreen() {
+
+    const styles = StyleSheet.create({
+        button: {
+            alignItems: "center",
+            backgroundColor: "#DDDDDD",
+            padding: 10,
+            width: '100%',
+        },
+        buttonText: {
+            fontSize: 20
+        },  
+        buttonView: {
+            flex: 1,
+            justifyContent: 'flex-end'
+        }
+    });
+
+    const addNote = () => {
+        console.log("Adding Note");
+    }
+
     return (
-        <View>
+        <View style={{ flex: 1 }}>
             <Text>Notes List Screen</Text>
+            <View style={styles.buttonView}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={addNote}
+            >
+                <Text style={styles.buttonText}>Add Note</Text>
+            </TouchableOpacity>
+            </View>
         </View>
     )
 }
