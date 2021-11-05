c398b4c97e15b39827d656e6a466763b2dbf391a
trey-sumida <tasumida@hawaii.edu>
2021-11-04 21:33:53 -1000

    basic setup

--- App.js
+++ App.js
@@ -1,32 +1,41 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import CalendarListScreen from './ui/screens/CalendarListScreen';
+import CalendarGridScreen from './ui/screens/CalendarGridScreen';
+import NotesScreen from './ui/screens/NotesScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'List') {
+            iconName = 'list-outline';
+          } else if (route.name === 'Grid') {
+            iconName = 'grid-outline';
+          } else if (route.name === 'Notes') {
+            iconName = 'pencil-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+        <Tab.Screen name="List" component={CalendarListScreen} />
+        <Tab.Screen name="Grid" component={CalendarGridScreen} />
+        <Tab.Screen name="Notes" component={NotesScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ ui/screens/CalendarGridScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function CalendarGridScreen() {
+    return (
+        <View>
+            <Text>Calendar Grid</Text>
+        </View>
+    )
+}
