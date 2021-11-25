2b1857a3f70b9e3cc92a7fd07a29b53c9b78dcb1
trey-sumida <tasumida@hawaii.edu>
2021-11-24 21:02:42 -1000

    note gets deleted but isnt automatically removed from list
    the note list screen needs to be re-rendered for the list to be updated

--- ui/components/notes/NoteItem.js
+++ ui/components/notes/NoteItem.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, TouchableOpacity, StyleSheet, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function NoteItem({ title, navigation }) {
 
@@ -13,16 +16,43 @@ export default function NoteItem({ title, navigation }) {
         buttonText: {
             fontSize: 20,
             color: 'white'
-        },  
+        },
+        delete: {
+            backgroundColor: "red",
+            color: "white",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            width: 50
+        },
     });
 
     const openNote = () => {
         navigation.navigate("Note", {title: title});
     }
 
+    const deleteNote = async () => {
+        const data = await AsyncStorage.getItem("NOTES");
+        const notes = JSON.parse(data);
+        delete notes[title];
+        await AsyncStorage.setItem("NOTES", JSON.stringify(notes));
+    }
+
+    const renderRightActions = (_progress, _dragAnimatedValue) => {
+        return (
+            <TouchableOpacity onPress={deleteNote}>
+                <View style={styles.delete}>
+                    <Ionicons name="trash-bin-outline" size={30}/>
+                </View>
+            </TouchableOpacity>
+        );
+      };
+
     return (
-        <TouchableOpacity style={styles.button} onPress={openNote}>
-            <Text style={styles.buttonText}>{title}</Text>
-        </TouchableOpacity>
+        <Swipeable renderRightActions={renderRightActions} key={title}>
+            <TouchableOpacity style={styles.button} onPress={openNote}>
+                <Text style={styles.buttonText}>{title}</Text>
+            </TouchableOpacity>
+        </Swipeable>
     )
 }
--- ui/components/notes/NotesScrollView.js
+++ ui/components/notes/NotesScrollView.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react'
 import { useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ScrollView, View, StyleSheet, TouchableOpacity } from 'react-native'
-import { Ionicons } from '@expo/vector-icons';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { ScrollView, View, StyleSheet } from 'react-native'
 import NoteItem from './NoteItem';
 
 export default function NotesScrollView({ navigation }) {
@@ -26,32 +24,14 @@ export default function NotesScrollView({ navigation }) {
         view: {
             flex: 1,
             height: "100%"
-        }, 
-        delete: {
-            backgroundColor: "red",
-            color: "white",
-            height: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-            width: 50
         }
       });
-
-      const renderRightActions = (_progress, _dragAnimatedValue) => {
-        return (
-            <TouchableOpacity onPress={()=>{console.log(`deleting ${notes}`)}}>
-                <View style={styles.delete}>
-                    <Ionicons name="trash-bin-outline" size={30}/>
-                </View>
-            </TouchableOpacity>
-        );
-      };
 
     return (
         <View style={styles.view}>
             <ScrollView style={styles.scrollView}>
                 {notes ? Object.keys(notes).map(keyName => (
-                    <Swipeable renderRightActions={renderRightActions} key={keyName}><NoteItem title={keyName} navigation={navigation} key={keyName} id={keyName}/></Swipeable>
+                    <NoteItem title={keyName} navigation={navigation} key={keyName} id={keyName}/>
                 )) : null}
             </ScrollView>
         </View>
