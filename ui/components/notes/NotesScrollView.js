273fe5f397a14918a7ab8ef1ee4af25af25f6212
trey-sumida <tasumida@hawaii.edu>
2021-11-24 20:49:52 -1000

    added swipable with delete
    The delete functionality isn't implemented as of yet

--- ui/components/notes/NotesScrollView.js
+++ ui/components/notes/NotesScrollView.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import { useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ScrollView, View, StyleSheet } from 'react-native'
+import { ScrollView, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 import NoteItem from './NoteItem';
 
 export default function NotesScrollView({ navigation }) {
@@ -25,15 +27,31 @@ export default function NotesScrollView({ navigation }) {
             flex: 1,
             height: "100%"
         }, 
-        scrollView: {
-        },
+        delete: {
+            backgroundColor: "red",
+            color: "white",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            width: 50
+        }
       });
-      
+
+      const renderRightActions = (_progress, _dragAnimatedValue) => {
+        return (
+            <TouchableOpacity onPress={()=>{console.log(`deleting ${notes}`)}}>
+                <View style={styles.delete}>
+                    <Ionicons name="trash-bin-outline" size={30}/>
+                </View>
+            </TouchableOpacity>
+        );
+      };
+
     return (
         <View style={styles.view}>
             <ScrollView style={styles.scrollView}>
                 {notes ? Object.keys(notes).map(keyName => (
-                    <NoteItem title={keyName} navigation={navigation} key={keyName} id={keyName}/>
+                    <Swipeable renderRightActions={renderRightActions} key={keyName}><NoteItem title={keyName} navigation={navigation} key={keyName} id={keyName}/></Swipeable>
                 )) : null}
             </ScrollView>
         </View>
